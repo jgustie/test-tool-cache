17d15ec46ddd445ac396f0390143c996da2e088c
Jeremy Gustie <jeremy@stormforge.io>
2021-08-27 08:53:38 -0400

    Make action async

--- .github/actions/setup-operator-sdk/index.js
+++ .github/actions/setup-operator-sdk/index.js
@@ -1,29 +1,33 @@
 const tc = require('@actions/tool-cache');
 const core = require('@actions/core');
 
-try {
-  const versionSpec = core.getInput('operator-sdk-version');
-
-  let toolPath = tc.find('operatorSDK', versionSpec);
-  
-  if (!toolPath) {
-    core.info(`Attempting to download ${versionSpec}...`);
+async function run() {
+  try {
+    const versionSpec = core.getInput('operator-sdk-version');
     
-    let semver = '1.11.0';
-    let version = `v${semver}`;
+    let toolPath = tc.find('operatorSDK', versionSpec);
     
-    let os = process.platform;
-    let arch = process.arch;
-    if (arch == 'x64') {
-      arch = 'amd64';
+    if (!toolPath) {
+      core.info(`Attempting to download ${versionSpec}...`);
+      
+      let semver = '1.11.0';
+      let version = `v${semver}`;
+      
+      let os = process.platform;
+      let arch = process.arch;
+      if (arch == 'x64') {
+        arch = 'amd64';
+      }
+      
+      const downloadPath = await tc.downloadTool(`https://github.com/operator-framework/operator-sdk/releases/download/${version}/operator-sdk_${os}_${arch}`);
+      
+      toolPath = await tc.cacheFile(downloadPath, 'operator-sdk', 'operatorSDK', version);
     }
     
-    const downloadPath = await tc.downloadTool(`https://github.com/operator-framework/operator-sdk/releases/download/${version}/operator-sdk_${os}_${arch}`);
-    
-    toolPath = await tc.cacheFile(downloadPath, 'operator-sdk', 'operatorSDK', version);
+    core.addPath(toolPath);
+  } catch (error) {
+    core.setFailed(error.message);
   }
-  
-  core.addPath(toolPath);
-} catch (error) {
-  core.setFailed(error.message);
 }
+
+run();
