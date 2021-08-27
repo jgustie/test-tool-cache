987f823e55b8825b9bf86da87e7f62d7e876f6b2
Jeremy Gustie <jeremy@stormforge.io>
2021-08-27 11:06:53 -0400

    Look at releases

--- .github/actions/setup-operator-sdk/index.js
+++ .github/actions/setup-operator-sdk/index.js
@@ -2,23 +2,33 @@ const tc = require('@actions/tool-cache');
 const core = require('@actions/core');
 const fs = require('fs');
 const { Octokit } = require('@octokit/rest')
+const semver = require('semver')
 
 async function run() {
   try {
     const versionSpec = core.getInput('operator-sdk-version');
     let toolPath = tc.find('operatorSDK', versionSpec);
     if (!toolPath) {
-      const octokit = new Octokit();
-      const releases = await octokit.rest.repos.listReleases({owner: 'operator-framework', repo: 'operator-sdk'});
-      core.info('GOT RELEASES: ' + JSON.stringify(releases));
-      
-      // TODO Need to resolve versionSpec to version...
-      let version = 'v1.11.0';
-      
       let os = process.platform;
       let arch = process.arch;
       if (arch == 'x64') {
         arch = 'amd64';
+      }
+    
+      let version = '';
+      const octokit = new Octokit();
+      const releases = await octokit.rest.repos.listReleases({owner: 'operator-framework', repo: 'operator-sdk'});
+      for (const release of releases.data) {
+        if (release.prerelease) {
+          continue;
+        }
+        if (versionSpec == 'latest' || semver.satisfies(version, versionSpec)) {
+          version = release.name;
+          break;
+        }
+      }
+      if (!version) {
+        throw new Error(`Unable to resolve version ${versionSpec}`);
       }
       
       core.info(`Attempting to download ${version} (${os}/${arch})...`);
